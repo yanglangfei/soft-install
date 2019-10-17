8462d277a40dfa697160c6bf1343e84af8d5dbf1
yanglf <899117>
2019-10-17 16:17:21 +0800

    1

--- src/main/java/com/yanglf/soft/SoftInstallApplication.java
+++ src/main/java/com/yanglf/soft/SoftInstallApplication.java
@@ -1,13 +1,21 @@
 package com.yanglf.soft;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author yanglf
+ */
 @SpringBootApplication
-public class SoftInstallApplication {
+public class SoftInstallApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(SoftInstallApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
